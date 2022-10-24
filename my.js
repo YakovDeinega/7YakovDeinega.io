c57c209a3a3b63e96157997bbd069a78ccffb63a
YakovDeinega <113457795+YakovDeinega@users.noreply.github.com>
2022-10-24 10:05:28 +0300

    Create my.js

---
+++ my.js
@@ -0,0 +1,20 @@
+$(function(){
+  $('.pic-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+    ]
+  });  
+})
